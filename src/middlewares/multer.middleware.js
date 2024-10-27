6bba3d09ac494296eae26322a6e1542378bfa77c
vanshtyagi04 <vanshtyagi4526@gmail.com>
2024-10-27 12:26:11 +0530

    multer config

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,18 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./public/temp");
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        const ext = path.extname(file.originalname);
+        const name = path.basename(file.originalname, ext);
+        cb(null, `${name}-${uniqueSuffix}${ext}`);
+    }
+});
+
+export const upload = multer({ 
+    storage 
+});
